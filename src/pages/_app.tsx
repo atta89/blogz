f1faafa285e7207eee470520c28b3727068c0128
Chandra Wijaya <chandraw8999@gmail.com>
2023-08-31 10:38:19 +0700

    feat(token): move token to .env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from "next/app";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { SnackbarProvider } from "notistack";
+import axios from "axios";
 
 import { Page } from "@/types";
 import { Layout as MainLayout } from "@/layouts";
 import { Theme } from "@/styles";
-import { SnackbarProvider } from "notistack";
-import axios from "axios";
+import { API_TOKEN } from "@/config";
 
 type Props = AppProps & {
   Component: Page;
@@ -13,9 +14,7 @@ type Props = AppProps & {
 
 // initialize axios
 axios.defaults.baseURL = "https://gorest.co.in/public/v2";
-axios.defaults.headers.common[
-  "Authorization"
-] = `Bearer 2dc3a6b1b6efcd0589e36b836d7765eccbebd5161311c956d5a8256a92181602`;
+axios.defaults.headers.common["Authorization"] = `Bearer ${API_TOKEN}`;
 
 function App({ Component: NextComponent, pageProps }: Props) {
   const Layout = NextComponent.layout ?? MainLayout;
